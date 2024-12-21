d1aa59e2611b6a3d1a11f2eebcf3a62df8f73c59
Tanu verma <tanikshaverma966@gmail.com>
2024-12-21 16:03:05 +0530

    web DESIGN

---
+++ script6.js
@@ -0,0 +1,39 @@
+
+
+gsap.from(".container .heading1 h1", {
+    y: -500,
+    duration: 2,
+    delay: 1,
+
+})
+
+gsap.from(".page1 h2",{
+    y:-200,
+    duration:2,
+    delay:1,
+})
+gsap.from(".page1 .btn", {
+    x: 500,
+    duration: 2,
+    delay: 2,
+})
+
+gsap.from(" .para  button", {
+    x: 500,
+    duration: 2,
+    delay: 2,
+})
+gsap.from(".container .heading1 h2", {
+    y: -500,
+    duration: 2,
+    delay: 2,
+
+})
+
+gsap.from(".page3 .btn4", {
+    x: -500,
+    duration: 2,
+    delay: 1,
+})
+
+
